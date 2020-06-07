8e118391a6897dc924c2eddf54dc2d1df1590238
Leo Kukharau <kuhorev.leonid@gmail.com>
2020-06-06 17:52:16 -0700

    tests added

--- tests/test_madlib.py
+++ tests/test_madlib.py
@@ -1,5 +1,15 @@
-from madlib_cli import __version__
+from textwrap import dedent
+
+from madlib_cli.madlib import read_file, replace_words
 
 
-def test_version():
-    assert __version__ == '0.1.0'
+class TestMadlib:
+    def test_read_file(self):
+        expected = 'I am a test file'
+        actual = read_file('tests/test_file.txt')
+        assert actual == expected
+
+    def test_replace_words(self):
+        test_string = """Do not replace me{replace me}{replace me too}{and me as well 123}"""
+        expected = 'Do not replace me123'
+        actual = replace_words('{[^\}]*}', ['1', '2', '3'], test_string)
